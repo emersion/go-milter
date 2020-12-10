1025c0e3ac260ff30bcc65d4ebf6a8e77a109e07
Simon Ser <contact@emersion.fr>
2020-12-10 11:40:24 +0100

    Sync constants with Postfix
    See: https://github.com/vdukhovni/postfix/blob/7747625a8c715f2bdde6ccc0a8a6c8be92ae4ff5/postfix/src/milter/milter8.c

--- message.go
+++ message.go
@@ -9,26 +9,30 @@ type Message struct {
 type ActionCode byte
 
 const (
-	ActAccept    ActionCode = 'a'
-	ActContinue  ActionCode = 'c'
-	ActDiscard   ActionCode = 'd'
-	ActReject    ActionCode = 'r'
-	ActTempFail  ActionCode = 't'
-	ActReplyCode ActionCode = 'y'
-	ActSkip      ActionCode = 's'
+	ActAccept    ActionCode = 'a' // SMFIR_ACCEPT
+	ActContinue  ActionCode = 'c' // SMFIR_CONTINUE
+	ActDiscard   ActionCode = 'd' // SMFIR_DISCARD
+	ActReject    ActionCode = 'r' // SMFIR_REJECT
+	ActTempFail  ActionCode = 't' // SMFIR_TEMPFAIL
+	ActReplyCode ActionCode = 'y' // SMFIR_REPLYCODE
+
+	// [v6]
+	ActSkip ActionCode = 's' // SMFIR_SKIP
 )
 
 type ModifyActCode byte
 
 const (
-	ActAddRcpt      ModifyActCode = '+'
-	ActDelRcpt      ModifyActCode = '-'
-	ActReplBody     ModifyActCode = 'b'
-	ActAddHeader    ModifyActCode = 'h'
-	ActChangeHeader ModifyActCode = 'm'
-	ActInsertHeader ModifyActCode = 'i'
-	ActChangeFrom   ModifyActCode = 'e'
-	ActQuarantine   ModifyActCode = 'q'
+	ActAddRcpt      ModifyActCode = '+' // SMFIR_ADDRCPT
+	ActDelRcpt      ModifyActCode = '-' // SMFIR_DELRCPT
+	ActReplBody     ModifyActCode = 'b' // SMFIR_ACCEPT
+	ActAddHeader    ModifyActCode = 'h' // SMFIR_ADDHEADER
+	ActChangeHeader ModifyActCode = 'm' // SMFIR_CHGHEADER
+	ActInsertHeader ModifyActCode = 'i' // SMFIR_INSHEADER
+	ActQuarantine   ModifyActCode = 'q' // SMFIR_QUARANTINE
+
+	// [v6]
+	ActChangeFrom ModifyActCode = 'e' // SMFIR_CHGFROM
 )
 
 type Code byte
@@ -47,7 +51,9 @@ const (
 	CodeEOB    Code = 'E' // SMFIC_BODYEOB
 	CodeAbort  Code = 'A' // SMFIC_ABORT
 	CodeData   Code = 'T' // SMFIC_DATA
-	CodeQuitNC Code = 'K' // SMFIC_QUIT_NC
+
+	// [v6]
+	CodeQuitNewConn Code = 'K' // SMFIC_QUIT_NC
 )
 
 const MaxBodyChunk = 65535
--- milter.go
+++ milter.go
@@ -5,15 +5,19 @@ package milter
 // Multiple options can be set using a bitmask.
 type OptAction uint32
 
-// set which actions the milter wants to perform
+// Set which actions the milter wants to perform.
 const (
-	OptAddHeader    OptAction = 0x01
-	OptChangeBody   OptAction = 0x02
-	OptAddRcpt      OptAction = 0x04
-	OptRemoveRcpt   OptAction = 0x08
-	OptChangeHeader OptAction = 0x10
-	OptQuarantine   OptAction = 0x20
-	OptChangeFrom   OptAction = 0x40
+	OptAddHeader    OptAction = 1 << 0 // SMFIF_ADDHDRS
+	OptChangeBody   OptAction = 1 << 1 // SMFIF_CHGBODY
+	OptAddRcpt      OptAction = 1 << 2 // SMFIF_ADDRCPT
+	OptRemoveRcpt   OptAction = 1 << 3 // SMFIF_DELRCPT
+	OptChangeHeader OptAction = 1 << 4 // SMFIF_CHGHDRS
+	OptQuarantine   OptAction = 1 << 5 // SMFIF_QUARANTINE
+
+	// [v6]
+	OptChangeFrom      OptAction = 1 << 6 // SMFIF_CHGFROM
+	OptAddRcptWithArgs OptAction = 1 << 7 // SMFIF_ADDRCPT_PAR
+	OptSetSymList      OptAction = 1 << 8 // SMFIF_SETSYMLIST
 )
 
 // OptProtocol masks out unwanted parts of the SMTP transaction.
@@ -21,25 +25,33 @@ const (
 type OptProtocol uint32
 
 const (
-	OptNoConnect  OptProtocol = 0x01
-	OptNoHelo     OptProtocol = 0x02
-	OptNoMailFrom OptProtocol = 0x04
-	OptNoRcptTo   OptProtocol = 0x08
-	OptNoBody     OptProtocol = 0x10
-	OptNoHeaders  OptProtocol = 0x20
-	OptNoEOH      OptProtocol = 0x40
+	OptNoConnect  OptProtocol = 1 << 0 // SMFIP_NOCONNECT
+	OptNoHelo     OptProtocol = 1 << 1 // SMFIP_NOHELO
+	OptNoMailFrom OptProtocol = 1 << 2 // SMFIP_NOMAIL
+	OptNoRcptTo   OptProtocol = 1 << 3 // SMFIP_NORCPT
+	OptNoBody     OptProtocol = 1 << 4 // SMFIP_NOBODY
+	OptNoHeaders  OptProtocol = 1 << 5 // SMFIP_NOHDRS
+	OptNoEOH      OptProtocol = 1 << 6 // SMFIP_NOEOH
+	OptNoUnknown  OptProtocol = 1 << 8 // SMFIP_NOUNKNOWN
+	OptNoData     OptProtocol = 1 << 9 // SMFIP_NODATA
 
-	// [v6] MTA supports ActSkip.
-	OptSkip OptProtocol = 0x400
+	// [v6] MTA supports ActSkip
+	OptSkip OptProtocol = 1 << 10 // SMFIP_SKIP
+	// [v6] Filter wants rejected RCPTs
+	OptRcptRej OptProtocol = 1 << 11 // SMFIP_RCPT_REJ
 
-	// [v6] milter will not send action response for following MTA messages.
-	OptNoHeaderReply  OptProtocol = 0x80
-	OptNoConnReply    OptProtocol = 0x1000
-	OptNoHeloReply    OptProtocol = 0x2000
-	OptNoMailReply    OptProtocol = 0x4000
-	OptNoRcptReply    OptProtocol = 0x8000
-	OptNoDataReply    OptProtocol = 0x10000
-	OptNoUnknownReply OptProtocol = 0x20000
-	OptNoEOHReply     OptProtocol = 0x40000
-	OptNoBodyReply    OptProtocol = 0x80000
+	// Milter will not send action response for the following MTA messages
+	OptNoHeaderReply OptProtocol = 1 << 7 // SMFIP_NR_HDR, SMFIP_NOHREPL
+	// [v6]
+	OptNoConnReply    OptProtocol = 1 << 12 // SMFIP_NR_CONN
+	OptNoHeloReply    OptProtocol = 1 << 13 // SMFIP_NR_HELO
+	OptNoMailReply    OptProtocol = 1 << 14 // SMFIP_NR_MAIL
+	OptNoRcptReply    OptProtocol = 1 << 15 // SMFIP_NR_RCPT
+	OptNoDataReply    OptProtocol = 1 << 16 // SMFIP_NR_DATA
+	OptNoUnknownReply OptProtocol = 1 << 17 // SMFIP_NR_UNKN
+	OptNoEOHReply     OptProtocol = 1 << 18 // SMFIP_NR_EOH
+	OptNoBodyReply    OptProtocol = 1 << 19 // SMFIP_NR_BODY
+
+	// [v6]
+	OptHeaderLeadingSpace OptProtocol = 1 << 20 // SMFIP_HDR_LEADSPC
 )
