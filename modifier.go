66cc0fbd0b5ea990a71450f26c13bac4f5da1192
Simon Ser <contact@emersion.fr>
2021-01-12 17:24:25 +0100

    server: convert CRLF to LF
    Postfix expects LF. Using CRLF results in \r\r\n sequences.

--- modifier.go
+++ modifier.go
@@ -9,6 +9,11 @@ import (
 	"net/textproto"
 )
 
+// postfix wants LF lines endings. Using CRLF results in double CR sequences.
+func crlfToLF(b []byte) []byte {
+	return bytes.ReplaceAll(b, []byte{'\r', '\n'}, []byte{'\n'})
+}
+
 // Modifier provides access to Macros, Headers and Body data to callback handlers. It also defines a
 // number of functions that can be used by callback handlers to modify processing of the email message
 type Modifier struct {
@@ -32,13 +37,17 @@ func (m *Modifier) DeleteRecipient(r string) error {
 
 // ReplaceBody substitutes message body with provided body
 func (m *Modifier) ReplaceBody(body []byte) error {
+	body = crlfToLF(body)
 	return m.writePacket(NewResponse('b', body).Response())
 }
 
 // AddHeader appends a new email message header the message
 func (m *Modifier) AddHeader(name, value string) error {
-	data := []byte(name + null + value + null)
-	return m.writePacket(NewResponse('h', data).Response())
+	var buffer bytes.Buffer
+	buffer.WriteString(name + null)
+	buffer.Write(crlfToLF([]byte(value)))
+	buffer.WriteString(null)
+	return m.writePacket(NewResponse('h', buffer.Bytes()).Response())
 }
 
 // Quarantine a message by giving a reason to hold it
@@ -49,46 +58,32 @@ func (m *Modifier) Quarantine(reason string) error {
 // ChangeHeader replaces the header at the specified position with a new one.
 // The index is per name.
 func (m *Modifier) ChangeHeader(index int, name, value string) error {
-	buffer := new(bytes.Buffer)
-	// encode header index in the beginning
-	if err := binary.Write(buffer, binary.BigEndian, uint32(index)); err != nil {
+	var buffer bytes.Buffer
+	if err := binary.Write(&buffer, binary.BigEndian, uint32(index)); err != nil {
 		return err
 	}
-	// add header name and value to buffer
-	data := []byte(name + null + value + null)
-	if _, err := buffer.Write(data); err != nil {
-		return err
-	}
-	// prepare and send response packet
+	buffer.WriteString(name + null)
+	buffer.Write(crlfToLF([]byte(value)))
+	buffer.WriteString(null)
 	return m.writePacket(NewResponse('m', buffer.Bytes()).Response())
 }
 
 // InsertHeader inserts the header at the specified position
 func (m *Modifier) InsertHeader(index int, name, value string) error {
-	buffer := new(bytes.Buffer)
-	// encode header index in the beginning
-	if err := binary.Write(buffer, binary.BigEndian, uint32(index)); err != nil {
+	var buffer bytes.Buffer
+	if err := binary.Write(&buffer, binary.BigEndian, uint32(index)); err != nil {
 		return err
 	}
-	// add header name and value to buffer
-	data := []byte(name + null + value + null)
-	if _, err := buffer.Write(data); err != nil {
-		return err
-	}
-	// prepare and send response packet
+	buffer.WriteString(name + null)
+	buffer.Write(crlfToLF([]byte(value)))
+	buffer.WriteString(null)
 	return m.writePacket(NewResponse('i', buffer.Bytes()).Response())
 }
 
 // ChangeFrom replaces the FROM envelope header with a new one
 func (m *Modifier) ChangeFrom(value string) error {
-	buffer := new(bytes.Buffer)
-	// add header name and value to buffer
 	data := []byte(value + null)
-	if _, err := buffer.Write(data); err != nil {
-		return err
-	}
-	// prepare and send response packet
-	return m.writePacket(NewResponse('e', buffer.Bytes()).Response())
+	return m.writePacket(NewResponse('e', data).Response())
 }
 
 // newModifier creates a new Modifier instance from milterSession
